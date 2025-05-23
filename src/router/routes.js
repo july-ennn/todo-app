9c71b0061da84fad186a331f44b6aeae706362a3
Julie <julieformalejo@gmail.com>
2025-05-23 14:32:02 +0800

    add todo page in routing

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,12 +3,12 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: 'todo', component: () => import('pages/ToDoPage.vue') }
     ]
   },
 
-  // Always leave this as last one,
-  // but you can also remove it
+  // Catch-all for 404s
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue')
